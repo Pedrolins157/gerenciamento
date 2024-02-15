2469a39e58b7e59c21be9a5da1f8e9c3e199aecc
Pedrolins157 <sgtlins02@gmail.com]>
2024-02-15 18:38:15 -0300

    Primeiro commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu Projeto Laravel</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Conteúdo do seu aplicativo Laravel -->
+@yield('content')
+
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
